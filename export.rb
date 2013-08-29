09cfa2df8f61c4a10789eeb6ffce1224d4075d3c
amy <amy@zendesk.com>
2013-08-29 10:14:51 +0800

    User#find_or_create_by_email

--- export.rb
+++ export.rb
@@ -13,6 +13,24 @@ CSV.foreach(csv_filename, :headers=>true, :header_converters=>:symbol) do |row|
   c = Case.new row[:case_id], row[:subject], row[:description], row[:created_at], row[:resolved_at], row[:type], row[:status], row[:priority], row[:labels]
   c.save
 
+  agent = User.find_or_create_by_email row[:agent]
+  agent.act_as_agent row[:group_name]
+  agent.save
+
+  user = User.find_or_create_by_email row[:requestor]
+
+  puts "CASE"
+  puts c.inspect
+  puts
+
+  puts "Agent"
+  puts agent.inspect
+  puts
+
+  puts "User"
+  puts user.inspect
+  puts
+
   count += 1
   break if count >= max
 end
--- user.rb
+++ user.rb
@@ -2,13 +2,13 @@ class User
   attr_reader :id, :email, :group_name
   def initialize email
     @email = email
-    @id = self.class.next_id
     @type = 'user'
   end
 
   def save
     if !(self.class.find_by_email @email)
       self.class.save_by_email self
+      @id = self.class.next_id
     end
   end
 
@@ -27,6 +27,15 @@ class User
 
   def self.find_by_email email
     self.storage[email]
+  end
+
+  def self.find_or_create_by_email email
+    existed = self.find_by_email email
+    return existed if existed
+
+    newly_created = self.new email
+    newly_created.save
+    newly_created
   end
 
   def self.save_by_email u
