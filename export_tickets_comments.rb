6ec36f0f3937c7d8e01daa13dbfd5fc908680043
amy <amy@zendesk.com>
2013-08-30 00:18:40 +0800

    use default user if author is null

--- export_tickets_comments.rb
+++ export_tickets_comments.rb
@@ -12,10 +12,6 @@ if csv_filenames.last.to_i.to_s==csv_filenames.last
   max = csv_filenames.pop.to_i # per file max
 end
 
-puts "filenames: #{csv_filenames.inspect}"
-puts "max: #{max}"
-
-
 outfile = File.open('./Tickets Comments.csv', "wb")
 outfile << (["Ticket #", "Ticket Comment #", "Comment", "Creation Date [US]", "Author [id]", "Public"].join(', '))
 outfile << "\n"
@@ -27,13 +23,15 @@ default_user = User.find_or_create_by_email 'dummy_user@test_for_box.earth'
 default_user.save
 
 csv_filenames.each do |csv_filename|
+  puts "Processing #{csv_filename}"
   count = 0 # max is per file
   CSV.foreach(csv_filename, :headers=>true, :header_converters=>:symbol) do |row|
     message = Message.new row[:case_id], row[:message_id], row[:message], row[:creation_date], row[:author], row[:public]
     message.save
 
     # check to see if author is 'customer'
-    if row[:author].downcase != "customer"
+    if (row[:author].downcase != "customer") &&
+       (row[:author].downcase != "null")
       author = User.find_or_create_by_email row[:author]
     else
       author = default_user
