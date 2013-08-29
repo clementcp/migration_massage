ace585ad10e36a008d3b64828203e3c326cad554
amy <amy@zendesk.com>
2013-08-29 17:14:16 +0800

    export agents to groups.csv

---
+++ export_groups.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require './user.rb'
+require './string_extension.rb'
+
+outfile = File.open('./Groups.csv', "wb")
+outfile << (["Name", "Agent"].join(', '))
+outfile << "\n"
+
+User.load_storage
+
+User.storage.each_pair do |email, user|
+  # Write to output csv
+  # Skip if Twitter user
+  next if user.twitter?
+  next if user.type == 'user'
+
+  quoted = Array.new
+  [user.group_name, user.email].each do |element|
+    quoted << element.to_s.quote
+  end
+  outfile << quoted.join(',')
+  outfile << "\n"
+end
+outfile.close
+
