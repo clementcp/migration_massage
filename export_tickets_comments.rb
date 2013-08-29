be1ca3e7a074bbc22e29560f5de153be247b8bba
amy <amy@zendesk.com>
2013-08-29 14:49:17 +0800

    double quote all element

--- export_tickets_comments.rb
+++ export_tickets_comments.rb
@@ -5,14 +5,24 @@ require './case.rb'
 require './user.rb'
 require './message.rb'
 
+class String
+  def quote
+    "\"#{self}\""
+  end
+end
+
 csv_filename = ARGV.shift
 max = ARGV.shift.to_i
 count = 0
 
 # Write header file
 out_filename = './Tickets Comments.csv'
-out_csv = CSV.open(out_filename, "wb")
-out_csv << ["Ticket #", "Ticket Comment #", "Comment", "Creation Date [US]", "Author [id]", "Public"]
+# out_csv = CSV.open(out_filename, "wb")
+# out_csv << ["Ticket #", "Ticket Comment #", "Comment", "Creation Date [US]", "Author [id]", "Public"]
+
+outfile = File.open(out_filename, "wb")
+outfile << (["Ticket #", "Ticket Comment #", "Comment", "Creation Date [US]", "Author [id]", "Public"].join(', '))
+outfile << "\n"
 
 CSV.foreach(csv_filename, :headers=>true, :header_converters=>:symbol) do |row|
   message = Message.new row[:case_id], row[:message_id], row[:message], row[:creation_date], row[:author], row[:public]
@@ -20,10 +30,16 @@ CSV.foreach(csv_filename, :headers=>true, :header_converters=>:symbol) do |row|
   author = User.find_or_create_by_email row[:author]
 
   # Write to output csv
-  out_csv << [message.case_id, message.id, message.body, message.created_at, author.id, message.formatted_public]
+  quoted = Array.new
+  [message.case_id, message.id, message.body, message.created_at, author.id, message.formatted_public].each do |element|
+    quoted << element.to_s.quote
+  end
+  outfile << quoted.join(',')
+  outfile << "\n"
 
   count += 1
   break if count >= max
 end
 
-out_csv.close
+outfile.close
+
