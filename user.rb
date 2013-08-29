430c910d2041c7c59b4714731e26f96d89963821
amy <amy@zendesk.com>
2013-08-29 10:00:06 +0800

    User can act as an agent

--- user.rb
+++ user.rb
@@ -1,14 +1,24 @@
 class User
-  attr_reader :id, :email
+  attr_reader :id, :email, :group_name
   def initialize email
     @email = email
     @id = self.class.next_id
+    @type = 'user'
   end
 
   def save
     if !(self.class.find_by_email @email)
       self.class.save_by_email self
     end
+  end
+
+  def act_as_agent group_name
+    @type = 'agent'
+    @group_name = group_name
+  end
+
+  def agent?
+    @type == 'agent'
   end
 
   def self.storage
